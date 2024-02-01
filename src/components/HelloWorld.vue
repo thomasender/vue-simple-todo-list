1da70574d405fff14b46a56367562f7778a1d338
thomasender <freisingen@yahoo.com>
2024-02-01 23:05:18 +0100

    vue todo list

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string, age: number }>()
+
+const toDos = ref<string[]>([])
+
+
+function addTodo(todo: string) {
+  toDos.value.push(todo)
+}
+
+function clearTodos() {
+  toDos.value = []
+}
+
+function deleteTodo(index) {
+  toDos.value.splice(index, 1)
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <input type="text" v-model="todo" />
+    <button @click="addTodo(todo)">Add</button>
+    <ul>
+      <li v-for="(todo, index) in toDos">
+        <span>{{ todo }}</span>
+        <button @click="deleteTodo(index)">Delete</button>
+      </li>
+    </ul>
+    <p v-if="toDos.length === 0">Add a Todo</p>
+    <button v-else @click="clearTodos()">Clear All Todos</button>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
